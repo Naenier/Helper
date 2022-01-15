ffa5f81e9db7c0cff0014c2eaff7f6556812397e
Naenier <kolderfard@gmail.com>
2022-01-15 08:29:03 +0300

    Bug Fixes

--- app.py
+++ app.py
@@ -30,13 +30,13 @@ def run():
 
     # Рассылка Telegram
     schedule.every().friday.at("13:30").do(sch_msg.chats_message_1)
-    schedule.every(2).hours.do(sch_msg.chats_message_2)
-    schedule.every(2).hours.do(sch_msg.chats_message_3)
-    schedule.every(2).hours.do(sch_msg.chats_message_4)
-    schedule.every(2).hours.do(sch_msg.chats_message_5)
-    schedule.every(2).hours.do(sch_msg.chats_message_6)
+    schedule.every(64).minutes.do(sch_msg.chats_message_2)
+    schedule.every(66).minutes.do(sch_msg.chats_message_3)
+    schedule.every(62).minutes.do(sch_msg.chats_message_4)
+    schedule.every(68).minutes.do(sch_msg.chats_message_5)
+    schedule.every(70).minutes.do(sch_msg.chats_message_6)
     schedule.every().friday.at("13:30").do(sch_msg.chats_message_7)
-    schedule.every(2).hours.do(sch_msg.chats_message_8)
+    schedule.every(60).minutes.do(sch_msg.chats_message_8)
 
     while True:
         schedule.run_pending()
