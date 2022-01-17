9320690f0ca9b206321a0009f6245aa312a57262
Naenier <kolderfard@gmail.com>
2022-01-17 07:36:42 +0300

    telegram_config.py - ID Fixes

--- telegram_config.py
+++ telegram_config.py
@@ -25,13 +25,5 @@ class Telegram_ID:
 	Rukishaaa: str = '458814686'  # Рукият Дибирова
 	gavrilova_v_smm: str = '1341248686'  # Ксения Гаврилова
 
-	notion: str = 'BAACAgIAAxkBAAECJmJh3O4Zu2JE6DMsW7gaPehSZIRS_gACsBQAAn99YUld-a9zUnEyTR4E'  # МК по Notion
-	instagram: str = 'BAACAgIAAxkBAAECJmRh3O_b5eYEwvjKQOPlflKL0J7XFwAC6xYAAk2AkUtgLulrBsiBoh4E'  # Check-Up Instagram Like-Centre
-	funnel: str = 'BQACAgIAAxkBAAECJmxh3PP9NnegkSx3ftLxuHgm66WF-QACchMAAg0LKUkWNqRb0gWjLh4E'  # Гайд по построению воронки продаж
-	woman: str = 'BAACAgIAAxkBAAECJm5h3PUhXAIOIZ56454qxeHgr9yeuwAC1hAAAtK4sEvpb1QeIWGiIB4E'  # Гайд по женской психике
-	gift: str = 'BQACAgIAAxkBAAECJnBh3PV4wAc0l35WoxyHaIXQbmeYpQACoBUAAhbW8UvYbO3wOAvNBh4E'  # Гайд по подарку по чертам лица
-	armor: str = 'BAACAgIAAxkBAAECJv5h3R0vZrGdMXC4V0rBTrqcMRJucwACwgwAAhxtkEu_pzXIZTRxSx4E'  # Шлюхоброня 2.0
-	telemetry: str = 'BQACAgIAAxkBAAECKaNh3zRnF3x1_gAB2Stgt8eTmnno4sUAAi8SAAJegflKp5UQq0wntx4eBA'  # Программа для спектрального анализа
-
 
 tg_id = Telegram_ID()
