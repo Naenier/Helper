6d8fb5b8d1b77d2e24d82acbc56b3e34b04f90c4
Naenier <kolderfard@gmail.com>
2022-02-10 23:59:16 +0300

    Handlers Update

--- handlers.py
+++ handlers.py
@@ -294,7 +294,7 @@ def voice_chat_started(_, message):
 @app.on_message(filters.regex('self-presentation') & filters.me)
 def voice_chat_scheduled(_, message):
     app.delete_messages(chat_id=message.from_user.id, message_ids=message.message_id)
-    app.send_message(chat_id=message.from_user.id, text=msg.self_presentation)
+    app.send_message(chat_id=message.message_id, text=msg.self_presentation)
 
 
 @app.on_message(filters.regex('vc-scheduled') & filters.chat(tg_id.chat_id) & filters.me)
