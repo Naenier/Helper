ee17f87214bbbf85026bf4dc45e22f483a1afd78
Naenier <kolderfard@gmail.com>
2022-03-26 04:40:15 +0300

    Bug-Fixes

--- app.py
+++ app.py
@@ -9,17 +9,17 @@ from scheduler_tg import sch_msg
 
 def run():
     # Акселератор
-    schedule.every(1).monday.at("09:00").do(sch_msg.RnP)
-    schedule.every(1).sunday.at("04:37").do(sch_msg.Test)
+    schedule.every().monday.at("09:00").do(sch_msg.RnP)
+    schedule.every().sunday.at("04:42").do(sch_msg.Test)
 
     # Рассылка Telegram
-    schedule.every(1).friday.at("13:35").do(sch_msg.chats_message_1)
+    schedule.every().friday.at("13:35").do(sch_msg.chats_message_1)
     schedule.every(64).minutes.do(sch_msg.chats_message_2)
     schedule.every(66).minutes.do(sch_msg.chats_message_3)
     schedule.every(62).minutes.do(sch_msg.chats_message_4)
     schedule.every(68).minutes.do(sch_msg.chats_message_5)
     schedule.every(70).minutes.do(sch_msg.chats_message_6)
-    schedule.every(1).friday.at("13:30").do(sch_msg.chats_message_7)
+    schedule.every().friday.at("13:30").do(sch_msg.chats_message_7)
     schedule.every(60).minutes.do(sch_msg.chats_message_8)
 
     while True:
