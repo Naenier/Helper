e56a696305bd39b3236092c166230dbcc9bcabf0
Naenier <kolderfard@gmail.com>
2022-03-28 02:43:22 +0300

    New iter

--- handlers.py
+++ handlers.py
@@ -19,7 +19,7 @@ db = user_database('user_database.db')
 def help_message(_, message):
     for member in app.iter_chat_members(chat_id=tg_id.chat_id):
         app.send_message("me", member.user.username)
-        sleep(0.3)
+        sleep(1)
 
 
 @app.on_message(filters.regex("Mentoring X") & ~filters.me)
