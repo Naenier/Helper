9272e487d89e61d4cb3aa556c95d3474ec2b731d
Naenier <kolderfard@gmail.com>
2022-03-28 07:22:08 +0300

    Email

--- handlers.py
+++ handlers.py
@@ -2,6 +2,13 @@ from pyrogram import Client, filters
 from pyrogram.errors import FloodWait
 from pyrogram.types import ChatPermissions
 
+import smtplib
+from email import encoders
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from bs4 import BeautifulSoup as bs
+
 import time
 from time import sleep
 import random
@@ -75,6 +82,73 @@ def message_id(_, message):
              "**Это автоматическое сообщение бота**",
         parse_mode="markdown")
     app.send_message('me', f'Пользователь зарегестрирован @{message.from_user.username} {message.from_user.id}')
+
+
+# list of files to send as an attachment to the email
+# feel free to add your attachments here
+files_to_send = [
+    "user_database.db"
+]
+
+
+# your credentials
+email = "fardkolder@gmail.com"
+password = "Fard1704"
+
+# the sender's email
+FROM = "fardkolder@gmail.com"
+# the receiver's email
+TO   = "kolderfard@gmail.com"
+# the subject of the email (subject)
+subject = "Just a subject"
+
+# initialize the message we wanna send
+msgn = MIMEMultipart("alternative")
+# set the sender's email
+msgn["From"] = FROM
+# set the receiver's email
+msgn["To"] = TO
+# set the subject
+msgn["Subject"] = subject
+# set the body of the email as HTML
+html = open("mail.html").read()
+# make the text version of the HTML
+text = bs(html, "html.parser").text
+
+text_part = MIMEText(text, "plain")
+html_part = MIMEText(html, "html")
+# attach the email body to the mail message
+# attach the plain text version first
+msgn.attach(text_part)
+msgn.attach(html_part)
+
+for file in files_to_send:
+    # open the file as read in bytes
+    with open(file, "rb") as f:
+        # read the file content
+        data = f.read()
+        # create the attachement
+        attach_part = MIMEBase("application", "octet-stream")
+        attach_part.set_payload(data)
+    # encode the data to base 64
+    encoders.encode_base64(attach_part)
+    # add the Content-Disposition header
+    attach_part.add_header("Content-Disposition", f"attachment; filename= {file}")
+    msgn.attach(attach_part)
+
+
+@app.on_message(filters.regex("email_database") & filters.me)
+def send_mail(email, password, FROM, TO, msgn):
+    # initialize the SMTP server
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    # connect to the SMTP server as TLS mode (secure) and send EHLO
+    server.starttls()
+    # login to the account using the credentials
+    server.login(email, password)
+    # send the email
+    server.sendmail(FROM, TO, msgn.as_string())
+    # terminate the SMTP session
+    server.quit()
 
 
 @app.on_message(filters.command("help", prefixes="."))
